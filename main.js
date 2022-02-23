6bec8ca198b3dd7448feae5e76a393e30671c05a
schedrov <rocolusso@icloud.com>
2022-02-24 01:34:30 +0200

    initial

---
+++ main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let firstNumber = Number(prompt('Введите первое число'));
+let secondNumber = Number(prompt('Введите второе число'));
+
+function slozhenie() {
+    let summa = firstNumber + secondNumber;
+    console.log( 'Сумма чисел : ' + summa);
+}
+
+slozhenie();
+
+function umnozhenie() {
+    let umnozh = firstNumber * secondNumber;
+    console.log( 'Произведение чисел : ' + umnozh);
+}
+
+umnozhenie();
+
+function delenie() {
+    let chastnoe = firstNumber / secondNumber;
+    console.log( 'Частное чисел : ' + chastnoe);
+}
+
+delenie();
