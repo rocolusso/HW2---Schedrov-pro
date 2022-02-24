5ec3e8bcae390a8faa9b36cfeb54465d9fa3136a
schedrov <rocolusso@icloud.com>
2022-02-24 02:07:27 +0200

    initial4

--- main.js
+++ main.js
@@ -5,28 +5,29 @@ let secondNumber = Number(prompt('Введите второе число'));
 
 function slozhenie() {
     let summa = firstNumber + secondNumber;
-    console.log( 'Сумма чисел : ' + summa);
+    console.log( 'Результат: ' +  firstNumber + ' + ' + secondNumber +  ' = ' + summa);
 }
 
 slozhenie();
 
 function vychitanie() {
     let raznitsa = firstNumber - secondNumber;
-    console.log( 'Разница чисел : ' + raznitsa);
+    console.log( 'Результат: ' +  firstNumber + ' - ' + secondNumber +  ' = ' + raznitsa);
+
 }
 
 vychitanie();
 
 function umnozhenie() {
     let umnozh = firstNumber * secondNumber;
-    console.log( 'Произведение чисел : ' + umnozh);
+    console.log( 'Результат: ' +  firstNumber + ' * ' + secondNumber +  ' = ' + umnozh);
 }
 
 umnozhenie();
 
 function delenie() {
     let chastnoe = firstNumber / secondNumber;
-    console.log( 'Частное чисел : ' + chastnoe);
+    console.log( 'Результат: ' +  firstNumber + ' / ' + secondNumber +  ' = ' + chastnoe);
 }
 
 delenie();
